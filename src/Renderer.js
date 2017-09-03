c1ebf7355419900a71d6c7a11e03ab506eb35797
Shota Matsuda <shota@takram.com>
2017-09-03 15:09:31 +0900

    added save/restore options and framewise uniforms

--- src/Renderer.js
+++ src/Renderer.js
@@ -32,8 +32,8 @@ export default class Renderer extends Three.WebGLRenderer {
   constructor(...args) {
     super(...args)
     const scope = internal(this)
-
-    // We're moving render functions to the prototype
+    scope.framewiseUniforms = {}
+    scope.options = {}
     scope.render = this.render.bind(this)
     scope.renderBufferDirect = this.renderBufferDirect.bind(this)
     scope.renderBufferImmediate = this.renderBufferImmediate.bind(this)
@@ -44,29 +44,94 @@ export default class Renderer extends Three.WebGLRenderer {
 
   render(scene, camera, renderTarget, forceClear) {
     const scope = internal(this)
-    scope.defaultUniforms = {
-      cameraZoom: camera.zoom || 1,
-      pixelRatio: this.getPixelRatio(),
-    }
+    scope.framewiseUniforms = this.createFramewiseUniforms(scene, camera)
     scope.render(scene, camera, renderTarget, forceClear)
   }
 
   renderBufferDirect(camera, fog, geometry, material, object, group) {
     const scope = internal(this)
-    const uniforms = material.uniforms
-    if (uniforms) {
-      if (uniforms.cameraZoom) {
-        uniforms.cameraZoom.value = scope.defaultUniforms.cameraZoom
-      }
-      if (uniforms.pixelRatio) {
-        uniforms.pixelRatio.value = scope.defaultUniforms.pixelRatio
-      }
-    }
+    this.applyFramewiseUniforms(material)
     scope.renderBufferDirect(camera, fog, geometry, material, object, group)
   }
 
   renderBufferImmediate(object, program, material) {
     const scope = internal(this)
+    this.applyFramewiseUniforms(material)
     scope.renderBufferImmediate(object, program, material)
+  }
+
+  createFramewiseUniforms(scene, camera) {
+    return {
+      cameraZoom: camera.zoom || 1,
+      pixelRatio: this.getPixelRatio(),
+    }
+  }
+
+  applyFramewiseUniforms(material) {
+    if (!material) {
+      return
+    }
+    const uniforms = material.uniforms
+    if (!uniforms) {
+      return
+    }
+    const scope = internal(this)
+    const names = Object.keys(scope.framewiseUniforms)
+    for (let i = 0; i < names.length; ++i) {
+      const uniform = uniforms[name]
+      if (uniform) {
+        uniform.value = scope.framewiseUniforms[name]
+      }
+    }
+  }
+
+  saveOptions() {
+    const scope = internal(this)
+    const options = scope.options
+  	options.autoClear = this.autoClear
+  	options.autoClearColor = this.autoClearColor
+  	options.autoClearDepth = this.autoClearDepth
+  	options.autoClearStencil = this.autoClearStencil
+  	options.clippingPlanes = this.clippingPlanes
+  	options.gammaFactor = this.gammaFactor
+  	options.gammaInput = this.gammaInput
+  	options.gammaOutput = this.gammaOutput
+  	options.localClippingEnabled = this.localClippingEnabled
+  	options.maxMorphNormals = this.maxMorphNormals
+  	options.maxMorphTargets = this.maxMorphTargets
+  	options.physicallyCorrectLights = this.physicallyCorrectLights
+  	options.shadowMapEnabled = this.shadowMap.enabled
+  	options.shadowMapRenderReverseSided = this.shadowMap.renderReverseSided
+  	options.shadowMapRenderSingleSided = this.shadowMap.renderSingleSided
+  	options.shadowMapType = this.shadowMap.type
+  	options.sortObjects = this.sortObjects
+  	options.toneMapping = this.toneMapping
+  	options.toneMappingExposure = this.toneMappingExposure
+  	options.toneMappingWhitePoint = this.toneMappingWhitePoint
+  }
+
+  restoreOptions() {
+    const scope = internal(this)
+    const options = scope.options
+    this.autoClear = options.autoClear
+    this.autoClearColor = options.autoClearColor
+    this.autoClearDepth = options.autoClearDepth
+    this.autoClearStencil = options.autoClearStencil
+    this.clippingPlanes = options.clippingPlanes
+    this.gammaFactor = options.gammaFactor
+    this.gammaInput = options.gammaInput
+    this.gammaOutput = options.gammaOutput
+    this.localClippingEnabled = options.localClippingEnabled
+    this.maxMorphNormals = options.maxMorphNormals
+    this.maxMorphTargets = options.maxMorphTargets
+    this.physicallyCorrectLights = options.physicallyCorrectLights
+    this.shadowMap.enabled = options.shadowMapEnabled
+    this.shadowMap.renderReverseSided = options.shadowMapRenderReverseSided
+    this.shadowMap.renderSingleSided = options.shadowMapRenderSingleSided
+    this.shadowMap.type = options.shadowMapType
+    this.sortObjects = options.sortObjects
+    this.toneMapping = options.toneMapping
+    this.toneMappingExposure = options.toneMappingExposure
+    this.toneMappingWhitePoint = options.toneMappingWhitePoint
   }
 }
